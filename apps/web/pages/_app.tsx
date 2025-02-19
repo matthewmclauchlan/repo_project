dae209abc9aa73cb02da194ee72fafcdd9b3d822
mattymclauchlan <info@spanishholidayrentals.co.uk>
2025-02-20 00:20:19 +0100

    chanegd web routing

--- apps/web/pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import type { AppProps } from "next/app";
-import "../styles/globals.css"; // ✅ Ensure this path is correct
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
--- apps/web/pages/index.tsx
+++
@@ -1,4 +0,0 @@
-export default function Home() {
-    return <h1>Welcome to My Next.js App</h1>;
-  }
-  
