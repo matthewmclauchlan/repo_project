2b95e5ddc442264cd4c658727d9d4226ba7b206c
mattymclauchlan <info@spanishholidayrentals.co.uk>
2025-02-20 11:08:36 +0100

    Updated metro config, package files, and TypeScript settings

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -1,6 +1,16 @@
-// metro.config.js
 const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
 
-const config = getDefaultConfig(__dirname);
+const workspaceRoot = path.resolve(__dirname, "../..");
+const projectRoot = __dirname;
+
+const config = getDefaultConfig(projectRoot);
+
+config.watchFolders = [workspaceRoot];
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(workspaceRoot, "node_modules")
+];
+config.resolver.disableHierarchicalLookup = true;
 
 module.exports = config;
